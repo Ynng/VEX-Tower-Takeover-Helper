b09d9876aabe1f6e91852a351da6980b45b1e12a
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-11-22 21:43:14 -0500

    commit

--- index.js
+++ index.js
@@ -85,45 +85,67 @@ var cube_interactions = function (e) {
 
         if (e.type == "fc") {
             e.target.parentNode.children[0].innerHTML = ++cube_counter[temp]
-            if(cl.contains("remove-animation")){
-                cl.remove("remove-animation")
-            }
-            if (cl.contains("add-animation")) {
-                cl.remove("add-animation")
-                setTimeout(function () {
-                    cl.add("add-animation")
-                }, 5);
-            } else {
-                cl.add("add-animation")
-            }
-            add_animation_counter++
+            trigger_add_animation(e.target)
         } else if (e.type == "swd") {
             if (cube_counter[temp] > 0) {
                 e.target.parentNode.children[0].innerHTML = --cube_counter[temp]
-                if(cl.contains("add-animation")){
-                    cl.remove("add-animation")
-                }
-                if (cl.contains("remove-animation")) {
-                    cl.remove("remove-animation")
-                    setTimeout(function () {
-                        cl.add("remove-animation")
-                    }, 10);
-                } else {
-                    cl.add("remove-animation")
-                }
+                trigger_remove_animation(e.target)
             }
-            remove_animation_counter++
         }
         update_score()
     }
 };
 
+
+var trigger_add_animation = function(e){
+    var cl = e.classList
+    if(cl.contains("remove-animation")){
+        cl.remove("remove-animation")
+    }
+    if (cl.contains("add-animation")) {
+        cl.remove("add-animation")
+        setTimeout(function () {
+            cl.add("add-animation")
+        }, 5);
+    } else {
+        cl.add("add-animation")
+    }
+}
+
+
+var trigger_remove_animation = function(e){
+    var cl = e.classList
+    if(cl.contains("add-animation")){
+        cl.remove("add-animation")
+    }
+    if (cl.contains("remove-animation")) {
+        cl.remove("remove-animation")
+        setTimeout(function () {
+            cl.add("remove-animation")
+        }, 10);
+    } else {
+        cl.add("remove-animation")
+    }
+}
+
 var friendly_score = 0, enemy_score = 0 , last_friendly_score = 0, last_enemy_score = 0;
 var update_score = function(){
+    var friendly_score_display = document.getElementsByClassName("score-display")[0].children[0];
+    var enemy_score_display = document.getElementsByClassName("score-display")[0].children[1];
     friendly_score = cube_counter[0]*(cube_counter[6]+1) + cube_counter[1]*(cube_counter[7]+1) + cube_counter[2]*(cube_counter[8]+1)
     enemy_score = cube_counter[3]*(cube_counter[6]+1) + cube_counter[4]*(cube_counter[7]+1) + cube_counter[5]*(cube_counter[8]+1)
-    document.getElementsByClassName("score-display")[0].children[0].innerHTML = friendly_score
-    document.getElementsByClassName("score-display")[0].children[1].innerHTML = enemy_score
-    if(friendly_score)
+    friendly_score_display.innerHTML = friendly_score
+    enemy_score_display.innerHTML = enemy_score
+    if(friendly_score>last_friendly_score){
+        trigger_add_animation(friendly_score_display)
+    }else if(friendly_score<last_friendly_score){
+        trigger_remove_animation(friendly_score_display)
+    }
+    if(enemy_score>last_enemy_score){
+        trigger_add_animation(enemy_score_display)
+    }else if(enemy_score<last_enemy_score){
+        trigger_remove_animation(enemy_score_display)
+    }
+    last_friendly_score=friendly_score
     console.log(friendly_score, enemy_score)
 };
